49cb305df3a8d6924fff0a59ded885c91b092625
Dylan Michael Labat <dml252@live.com>
2019-02-05 03:40:11 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre
@@ -16,4 +17,24 @@ class Book
     @genre = genre
     GENRES << genre 
   end
+=======
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
+
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+  end
+>>>>>>> b3aa9d75f28d6e381dcd80a374e94e794ad67429
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -16,4 +17,21 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+=======
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  BRANDS = []
+
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+>>>>>>> b3aa9d75f28d6e381dcd80a374e94e794ad67429
 end
